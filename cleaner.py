0e43c2bd0ff3ac091b58e42c8cfbc59b9499d7a0
Reggie-Free-Bird <reginald@afreebird.org>
2026-04-18 14:13:49 -0400

    added print to cleaner py

--- cleaner.py
+++ cleaner.py
@@ -3,4 +3,5 @@ def clean_data(data):
     for flt in data[1:]:
         flt = float(flt.replace("\n", ""))
         clean_data.append(flt)
+        print('you have cleaned the data')
     return clean_data
